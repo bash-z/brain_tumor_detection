d775a0471803c7922e1178b760da3744d624e9a9
Hamzah Shah <hamzahrshah@gmail.com>
2021-04-21 18:59:31 -0400

    pre gcp

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -2,8 +2,8 @@ num_epochs = 20
 
 learning_rate = 1e-3
 
-batch_size = 7
+batch_size = 25
 
-num_classes = 2
+num_classes = 3
 
 img_size = 64
--- code/preprocess.py
+++ code/preprocess.py
@@ -9,28 +9,48 @@ import hyperparameters as hp
 class Data():
 
     def __init__(self):
-        self.y = os.listdir("../data/yes") # array of names of images (strings)
-        self.n = os.listdir("../data/no")
+        self.b1 = os.listdir("../data/Train/BENIGN")
+        self.b1 = self.set_filepaths(self.b1, "../data/Train/BENIGN/")
+        self.b2 = os.listdir("../data/Test/BENIGN")
+        self.b2 = self.set_filepaths(self.b2, "../data/Test/BENIGN/")
 
+        self.m1 = os.listdir("../data/Train/MALIGNANT")
+        self.m1 = self.set_filepaths(self.m1, "../data/Train/MALIGNANT/")
+        self.m2 = os.listdir("../data/Test/MALIGNANT")
+        self.m2 = self.set_filepaths(self.m2, "../data/Test/MALIGNANT/")
+
+        self.n1 = os.listdir("../data/Train/NORMAL")
+        self.n1 = self.set_filepaths(self.n1, "../data/Train/NORMAL/")
+        self.n2 = os.listdir("../data/Test/NORMAL")
+        self.n2 = self.set_filepaths(self.n2, "../data/Test/NORMAL/")
+
+
+
+        self.images = np.concatenate([self.b1, self.b2, self.m1, self.m2, self.n1, self.n2])
+        self.labels = np.concatenate(
+            [np.full(len(self.b1) + len(self.b2),2),
+                                            np.full(len(self.m1) + len(self.m2),1), np.full(len(self.n1) + len(self.n2),0)])
         
-        self.images = np.concatenate([self.y,self.n])
-        self.labels = np.concatenate([np.full(len(self.y),1),np.full(len(self.n),0)])
         self.data_sample = np.zeros((len(self.images),hp.img_size,hp.img_size,3))
 
         self.X_train = []
         self.X_test = []
         self.y_train = []
         self.y_test = []
+    
+
+    def set_filepaths(self, image_names, parent_path):
+        for i in range(len(image_names)):
+            image_names[i] = parent_path + image_names[i]
+        
+        return image_names
+
 
    
     def normalize(self):
-        for j,i in enumerate(self.images): # j is index, i is file path
-            if j < len(self.y):
-                filepath = "../data/yes/"
-            else:
-                filepath = "../data/no/"
-
-            img = Image.open(filepath + i)
+        for j,filepath in enumerate(self.images): # j is index, i is file path
+        
+            img = Image.open(filepath)
             img = img.resize((hp.img_size, hp.img_size))
             img = np.array(img, dtype=np.float32)
             img /= 255. # normalizing pixels to 0-1
@@ -51,7 +71,7 @@ class Data():
     def split_data(self):
 
         X_train, X_test, y_train, y_test = train_test_split(self.data_sample, 
-                                                self.labels, test_size=0.25, random_state=39)
+                                                self.labels, test_size=0.25, random_state=42)
 
         self.X_train = X_train
         self.X_test = X_test
