11419dd77d5501005c35dcc3d298278c8f502779
Hamzah Shah <hamzahrshah@gmail.com>
2021-04-21 22:43:06 -0400

    added callbacks

--- code/hyperparameters.py
+++ code/hyperparameters.py
@@ -7,3 +7,5 @@ batch_size = 35
 num_classes = 3
 
 img_size = 64
+
+max_num_weights = 5
--- code/main.py
+++ code/main.py
@@ -2,10 +2,25 @@ from model import Model
 import hyperparameters as hp
 from preprocess import Data
 import tensorflow as tf
+import os
+from tensorboard_utils import \
+        ImageLabelingLogger, CustomModelSaver
 
 
 
-def train(model):
+
+def train(model, checkpoint_path, logs_path):
+
+    callback_list = [
+        tf.keras.callbacks.TensorBoard(
+            log_dir='logs',
+            update_freq='batch',
+            profile_batch=0),
+        ImageLabelingLogger(logs_path, data),
+        CustomModelSaver(checkpoint_path, 1, hp.max_num_weights)
+    ]
+
+    
     model.fit(
         x=data.X_train,
         y=data.y_train,
@@ -13,7 +28,8 @@ def train(model):
         batch_size=hp.batch_size,
         shuffle=True,
         validation_split=0.20,
-        verbose=1
+        verbose=1,
+        callbacks=callback_list
     )
 
 def test(model):
@@ -25,6 +41,8 @@ def test(model):
 
 
 if __name__ == "__main__":
+    timestamp = time_now.strftime("%m%d%y-%H%M%S")
+
     data = Data()
     data.normalize()
     data.preproccess()
@@ -32,6 +50,10 @@ if __name__ == "__main__":
 
     model = Model()
     model(tf.keras.Input(shape=(hp.img_size, hp.img_size, 3)))
+    checkpoint_path = "checkpoints" + os.sep + \
+            "your_model" + os.sep + timestamp + os.sep
+    logs_path = "logs" + os.sep + "your_model" + \
+            os.sep + timestamp + os.sep
 
     model.summary()
 
@@ -41,7 +63,7 @@ if __name__ == "__main__":
         metrics=["sparse_categorical_accuracy"]
         )
     
-    train(model)
+    train(model, checkpoint_path, logs_path)
     # test(model)
     
 
